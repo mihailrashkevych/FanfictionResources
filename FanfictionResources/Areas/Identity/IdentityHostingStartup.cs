d24c0d2812397d8439430b1f590df5325f3db165
mihailrashkevych <mihailrash@gmail.com>
2021-06-16 14:34:06 +0300

    Initial commit

---
+++ FanfictionResources/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FanfictionResources.Data;
+using FanfictionResources.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FanfictionResources.Areas.Identity.IdentityHostingStartup))]
+namespace FanfictionResources.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
